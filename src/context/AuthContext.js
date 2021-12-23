1279c6a677a7b9c2d3bb9b38402c8a319a566619
Ashwani Kumar <ashwanikr160@gmail.com>
2021-12-23 06:19:34 +0530

    react strict mode

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,43 +1,51 @@
 import React, { useState, useContext, useEffect } from "react";
-import { onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider, sendPasswordResetEmail, confirmPasswordReset } from "@firebase/auth";
-import { auth, db } from "../firebase/firebaseConfig"
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  sendPasswordResetEmail,
+  confirmPasswordReset,
+} from "@firebase/auth";
+import { auth, db } from "../firebase/firebaseConfig";
 import { doc, getDoc, Timestamp, updateDoc } from "@firebase/firestore";
 
 const AuthContext = React.createContext();
 
 function useAuth() {
-  return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState({ currentUser: null, authIsReady: false });
 
   function signup(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password)
+    return createUserWithEmailAndPassword(auth, email, password);
   }
   function login(email, password) {
-    return signInWithEmailAndPassword(auth, email, password)
+    return signInWithEmailAndPassword(auth, email, password);
   }
   function loginWithGoogle() {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
   }
   function logOut() {
-    return signOut(auth)
+    return signOut(auth);
   }
   function forgotPassword(email) {
     return sendPasswordResetEmail(auth, email, {
-      url: "https://spicy-dragon.netlify.app"
+      url: "https://spicy-dragon.netlify.app",
     });
   }
   function resetPassword(oobCode, newPassword) {
-    return confirmPasswordReset(auth, oobCode, newPassword)
+    return confirmPasswordReset(auth, oobCode, newPassword);
   }
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (user) => {
-      setUser({ currentUser: user, authIsReady: true })
-      console.log("onAuthStateChanged user :- ", user);
+      setUser({ currentUser: user, authIsReady: true });
       try {
         if (user) {
           async function lastLoginUpdate() {
@@ -45,19 +53,18 @@ function AuthProvider({ children }) {
             const docSnap = await getDoc(docRef);
             if (docSnap.exists()) {
               updateDoc(doc(db, "users", user.uid), {
-                lastSignInTime: Timestamp.fromDate(new Date())
-              })
+                lastSignInTime: Timestamp.fromDate(new Date()),
+              });
             }
           }
           lastLoginUpdate();
         }
-      }
-      catch (error) {
+      } catch (error) {
         console.log("Failed to updated user's lastSignInTime ", error.message);
       }
-    })
-    return unSubscribe
-  }, [])
+    });
+    return unSubscribe;
+  }, []);
 
   const value = {
     ...user,
@@ -66,14 +73,10 @@ function AuthProvider({ children }) {
     logOut,
     loginWithGoogle,
     forgotPassword,
-    resetPassword
-  }
+    resetPassword,
+  };
 
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  )
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
-export { useAuth, AuthProvider }
+export { useAuth, AuthProvider };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
-import { LoadingProvider } from './context/LoadingContext';
-import { RecipeListProvider } from './context/RecipeListContext';
-import { combineProviders } from './utility_js/combineProviders'
-import { ModalProvider } from './context/ModalContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
+import { LoadingProvider } from "./context/LoadingContext";
+import { RecipeListProvider } from "./context/RecipeListContext";
+import { combineProviders } from "./utility_js/combineProviders";
+import { ModalProvider } from "./context/ModalContext";
 
 const Providers = combineProviders([
   BrowserRouter,
@@ -16,14 +16,14 @@ const Providers = combineProviders([
   ThemeProvider,
   AuthProvider,
   LoadingProvider,
-  ModalProvider
-])
+  ModalProvider,
+]);
 
 ReactDOM.render(
-  <Providers>
-    <App />
-  </Providers>
-  ,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
